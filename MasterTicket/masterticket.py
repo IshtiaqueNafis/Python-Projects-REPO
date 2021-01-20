ca76d3600113c10dace520cbd17438112a451b90
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-01-20 14:30:35 -0500

    added while loop  for tickets

--- MasterTicket/masterticket.py
+++ MasterTicket/masterticket.py
@@ -6,20 +6,30 @@ print(f"there are currently {tickets_remaining}")
 
 # gather the username and assgin to a new variable.
 
-user_name = input("what is your name? ")
-print(user_name)
-
-# pomot the userby name and how many tickets would they like
-tickets_quantity = input('how many tickets would you like?')
 
 # calulate price
-amount = int(tickets_quantity) * TICKET_PRICE
+while tickets_remaining:
 
-print(f"ticket price is {amount}")
+    user_name = input("what is your name? ")
+    print(user_name)
 
-# promot user if they want to proceed
+    # pomot the userby name and how many tickets would they like
+    tickets_quantity = input('how many tickets would you like?')
+    amount = int(tickets_quantity) * TICKET_PRICE
 
+    print(f"ticket price is {amount}")
 
+    # promot user if they want to proceed
+    user_input = input("do you want to proceed?")
+    if user_input.lower() == 'y':
+        print("SOLD")
+        tickets_remaining -= int(tickets_quantity);
+    else:
+        print(f"thank you anyways {user_name}")
 
+    if tickets_remaining == 0:
+        print("soryy we have no tickets left")
+    else:
+        print(f"we still have {tickets_remaining}")
 # if they want to proceed
-   # print(sold)
+# print(sold)
