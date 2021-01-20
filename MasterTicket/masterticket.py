62c22310e0460e70c374b893ffc14ad8d479a428
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-01-20 14:15:25 -0500

    added info for ticket and price an user input

---
+++ MasterTicket/masterticket.py
@@ -0,0 +1,25 @@
+TICKET_PRICE = 10  # this is the constanct price
+
+tickets_remaining = 100  # ticket remaning
+
+print(f"there are currently {tickets_remaining}")
+
+# gather the username and assgin to a new variable.
+
+user_name = input("what is your name? ")
+print(user_name)
+
+# pomot the userby name and how many tickets would they like
+tickets_quantity = input('how many tickets would you like?')
+
+# calulate price
+amount = int(tickets_quantity) * TICKET_PRICE
+
+print(f"ticket price is {amount}")
+
+# promot user if they want to proceed
+
+
+
+# if they want to proceed
+   # print(sold)
